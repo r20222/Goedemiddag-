9c11ecd86aacc25e803482e25553e186e47bfeea
r20222 <101579892+r20222@users.noreply.github.com>
2024-07-21 16:48:30 +0200

    Logo Goedemiddag :nail_care:

--- components/ProjectNavigation.vue
+++ components/ProjectNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <a href="https://www.goedemiddag.nl/" target="_blank"
-      ><img src="/public/GM_logo_donkerrood_02.svg" alt=""
+      ><img src="/public/GM_logo_donkerrood_02.svg" alt="Logo Goedemiddag!"
     /></a>
   </nav>
 </template>
